fb173de4302d5470ef6aef9b26f274b15ec097a6
Pieter <pieterbrandsen2@gmail.com>
2021-04-22 14:57:22 +0200

    Added suggestion discussion part

--- src/commands/suggestion.js
+++ src/commands/suggestion.js
@@ -2,15 +2,14 @@ const { MessageEmbed } = require('discord.js');
 
 const commandObject = require('../modules/languageConfig').get('commands', 'suggestion');
 
-const { commandText } = commandObject;
-const { returnText } = commandObject;
+const { text, commandText, returnText } = commandObject;
 
 module.exports = {
   name: commandText.name,
   description: commandText.description,
   permission: commandText.permission,
   options: commandText.options,
-  async execute(client, args, interaction, { member, config }) {
+  async execute(client, args, interaction, { member, config,channel }) {
     if (config.suggestion.enabled.toLowerCase() !== 'true') return new MessageEmbed().setTitle(returnText.suggestionNotEnabled.title);
 
     const suggestionChannel = client.channels.cache.get(config.suggestion.channelId);
@@ -35,7 +34,24 @@ module.exports = {
       await msg.react('👍');
       await msg.react('👎');
 
-      const embed = new MessageEmbed().setTitle(returnText.createdSuggestion.title).setDescription(returnText.createdSuggestion.description.replace('{{ suggestionChannelId }}', config.suggestionChannelId));
+      if (config.suggestion.discussionChannelEnabled) {
+        const suggestionDiscussionChannel = client.channels.cache.get(config.suggestion.discussionChannelId);
+        if (suggestionDiscussionChannel == null) {
+          channel.send(new MessageEmbed()
+          .setTitle(returnText.noSuggestionDiscussionChannelEmbed.title)
+          .setDescription(returnText.noSuggestionDiscussionChannelEmbed.description)
+          .setColor(config.colour));
+        }
+        else {
+          const discussionChannelMsg = new MessageEmbed()
+          .setTitle(text.suggestionDiscussion.title)
+          .setDescription(text.suggestionDiscussion.description.replace("{{ messageTitle }}",title).replace("{{ messageDescription }}",message))
+          .setFooter(returnText.suggestionFrom.replace('{{ displayName }}', member.displayName)).setColor(config.colour);
+          await suggestionDiscussionChannel.send(discussionChannelMsg);
+        }
+      }
+
+      const embed = new MessageEmbed().setTitle(returnText.createdSuggestion.title).setDescription(returnText.createdSuggestion.description.replace('{{ suggestionChannelId }}', config.suggestion.channelId));
       return embed;
     }
 
--- src/events/message.js
+++ src/events/message.js
@@ -9,8 +9,16 @@ const { logText } = eventObject;
 module.exports = {
   name: 'message',
   async execute(client, [message]) {
+    const member = message.member;
+    if (member.id == client.user.id) return;    
+
     if (message.channel.type === 'dm') {
       log.console(logText.dmMessage
+        .replace('{{ username }}', message.author.tag)
+        .replace('{{ cleanMessage }}', message.cleanContent));
+    }
+    else {
+      log.console(logText.normalMessage
         .replace('{{ username }}', message.author.tag)
         .replace('{{ cleanMessage }}', message.cleanContent));
     }
