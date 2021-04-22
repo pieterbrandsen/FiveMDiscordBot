1b99cd45171330464fe8f830597bff55f93f6ee2
Pieter <pieterbrandsen2@gmail.com>
2021-04-22 21:55:53 +0200

    Added new database models and now using mssql

--- src/index.js
+++ src/index.js
@@ -26,23 +26,72 @@ const log = new Logger({
 /**
  * storage
  */
-log.info('Using SQLite storage');
-const sequelize = new Sequelize({
-  dialect: 'sqlite',
-  storage: 'user/storage.db',
-  logging: log.debug,
-});
+let sequelize;
+if(config.storage.type === 'mssql') {
+	log.info('Connecting to MySQL database...');
+	sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+		dialect: 'mssql',
+		host: process.env.DB_HOST,
+		logging: log.debug,
+    enableArithAbort: true,
+	});
+} else {
+	log.info('Using SQLite storage');
+	sequelize = new Sequelize({
+		dialect: 'sqlite',
+		storage: 'user/storage.db',
+		logging: log.debug
+	});
+}
 
-class Setting extends Model {}
-Setting.init({
-  key: DataTypes.STRING,
-  value: DataTypes.STRING,
+class SuggestionConfig extends Model {}
+SuggestionConfig.init({
+  enabled: DataTypes.STRING,
+  channelId: DataTypes.STRING,
+  discussionChannelEnabled: DataTypes.STRING,
+  discussionChannelId: DataTypes.STRING,
+}, {
+	sequelize,
+	modelName: 'suggestionConfig'
+})
+
+class VerifyConfig extends Model {}
+VerifyConfig.init({
+  unverifiedRoleId: DataTypes.BIGINT,
+  verifiedRoleId: DataTypes.BIGINT
+}, {
+	sequelize,
+	modelName: 'verifyConfig'
+})
+
+class WelcomingConfig extends Model {}
+WelcomingConfig.init({
+  enabled: DataTypes.STRING,
+  messageType: DataTypes.STRING,
+  channelId: DataTypes.STRING,
+  message: DataTypes.STRING,
+}, {
+	sequelize,
+	modelName: 'welcomingConfig'
+})
+
+class Config extends Model {}
+Config.init({
+  guildId: DataTypes.BIGINT,
+  guildName: DataTypes.STRING,
+  embedColor: DataTypes.STRING,
+  suggestionConfigId: DataTypes.BIGINT,
+  verifyConfigId: DataTypes.BIGINT,
+  welcomingConfigId: DataTypes.BIGINT,
+  suggestionConfigId: DataTypes.BIGINT,
 }, {
   sequelize,
-  modelName: 'setting',
+  modelName: 'config',
 });
-
-Setting.sync();
+Config.sync();
+SuggestionConfig.sync();
+VerifyConfig.sync();
+WelcomingConfig.sync();
 
 /**
  * event loader
@@ -52,7 +101,7 @@ fs.readdirSync('src/events').filter((file) => file.endsWith('.js')).forEach((fil
   const event = require(`./events/${file}`);
   client.events.set(event.name, event);
   client.on(event.name, (e1, e2) => client.events.get(event.name).execute(client, [e1, e2], {
-    config, Setting,
+    config,Config
   }));
   log.console(log.format(`> Loaded &7${event.name}&f event`));
 });
@@ -60,7 +109,7 @@ fs.readdirSync('src/events').filter((file) => file.endsWith('.js')).forEach((fil
 log.info(`Loaded ${client.events.size} events`);
 
 client.ws.on('INTERACTION_CREATE', async (interaction) => {
-  executeCommand.execute(interaction, client, { config });
+  executeCommand.execute(interaction, client, { config, Config });
 });
 
 process.on('unhandledRejection', (error) => {
